e0db7d89281b5486bcdf54468aee851f9f4baeab
Slawomir Borowiec <przodownik@tlen.pl>
2016-07-13 00:23:39 +0200

    temp

--- src/main/java/pl/java/scalatech/config/SwaggerConfig.java
+++ src/main/java/pl/java/scalatech/config/SwaggerConfig.java
@@ -19,7 +19,7 @@ public class SwaggerConfig {
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
           .select()                                  
-          .apis(RequestHandlerSelectors.any())              
+          .apis(RequestHandlerSelectors.basePackage("pl.java.scalatech.web.controller"))              
           .paths(PathSelectors.any())                          
           .build();                                           
     }
